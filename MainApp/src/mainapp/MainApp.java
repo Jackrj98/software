d327aa306f7c855db3970d8bd46c9644673b7f9e
Jackrj98 <jack.rojas@unl.edu.ec>
2020-05-19 14:31:46 -0500

    Method Paint recursivo

--- MainApp/src/mainapp/MainApp.java
+++ MainApp/src/mainapp/MainApp.java
@@ -21,4 +21,19 @@ public class MainApp {
         paintRecursivo(g,nivel_de_recursividad,xp1,yp1,xp2,yp2);
     }
     
+    private void paintRecursivo(Graphics g, int i, double xp12, double yp12, double xp22, double yp22 ) {
+        double dx=(xp22-xp12)/3.;
+        double dy=(yp22-yp12)/3.;
+        double xx=xp12+3*dx/2.-dy*sin60;
+        double yy=yp12+3*dy/2.+dx*sin60;
+        if(i<=0){
+            g.drawLine((int)xp12,(int)yp12,(int)xp22,(int)yp22);
+        }else{
+            paintRecursivo(g,i-1,xp12,yp12,xp12+dx,yp12+dy);
+            paintRecursivo(g,i-1,xp12+dx,yp12+dy,xx,yy);
+            paintRecursivo(g,i-1,xx,yy,xp22-dx,yp22-dy);
+            paintRecursivo(g,i-1,xp22-dx,yp22-dy,xp22,yp22);
+        }
+    } 
 }
+
